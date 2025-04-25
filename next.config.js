dc367f02e79af73516859fdcd43cf3e2a856241d
Debsankar Pal <debsankarpal99@gmail.com>
2025-04-25 14:58:07 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Recommended for highlighting potential problems in an application
+  swcMinify: true, // Recommended for faster builds
+};
+
+module.exports = nextConfig;
