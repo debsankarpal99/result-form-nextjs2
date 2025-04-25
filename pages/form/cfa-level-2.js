71f03b4da477250d13a34652d4915a6f675af303
Debsankar Pal <debsankarpal99@gmail.com>
2025-04-25 14:53:18 +0530

    Update cfa-level-2.js

--- pages/form/cfa-level-2.js
+++ pages/form/cfa-level-2.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const CFALevel1Form = () => {
+  return (
+    <div className="container">
+      <h1>CFA Level 2 Form Page</h1>
+      <p>This is the form for CFA Level 1.</p>
+      {/* Add the rest of your form fields for this section */}
+    </div>
+  );
+};
+
+export default CFALevel1Form;
